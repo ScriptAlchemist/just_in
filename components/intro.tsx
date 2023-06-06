65674cbaeb563cd91a1c3270602322eca69fb8b5
Cardano G.O.A.T <Jrkbender@gmail.com>
2023-06-06 13:44:22 -0400

    changing name to some(scripting)

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => {
     <>
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Just<span className="text-red-500">(</span>in<span className="text-red-500">)</span>
+        Some<span className="text-red-500">(</span>Scripting<span className="text-red-500">)</span>
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         <div>
