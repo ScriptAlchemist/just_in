ff4599b9655b125d1cd693917142a4997a1668e7
Cardano G.O.A.T <Jrkbender@gmail.com>
2023-06-06 05:57:49 -0400

    rehyping the markup to get labels for styling code blocks

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,22 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import {unified} from 'unified'
+//import stream from 'unified-stream'
+import remarkParse from 'remark-parse'
+//import remarkToc from 'remark-toc'
+import remarkRehype from 'remark-rehype'
+import rehypeDocument from 'rehype-document'
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify'
+import rehypeHighlight from 'rehype-highlight'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await unified()
+    .use(remarkParse)
+    //.use(remarkToc)
+    .use(remarkRehype)
+    .use(rehypeDocument, {title: 'Contents'})
+    .use(rehypeFormat)
+    .use(rehypeHighlight)
+    .use(rehypeStringify)
+    .process(markdown);
   return result.toString()
 }
