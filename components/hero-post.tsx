d51651b2a00bc1b0b5f75a21333c65c88fc22c46
Cardano G.O.A.T <Jrkbender@gmail.com>
2023-06-07 21:34:05 -0400

    adding crap picture and updating hero-post section so the image is not so overwhelming.

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -22,11 +22,11 @@ const HeroPost = ({
   slug,
 }: Props) => {
   return (
-    <section>
-      <div className="mb-8 md:mb-16">
+    <section className="flex flex-col md:flex-row items-center">
+      <div className="mb-8 md:mb-16 w-[70%] mx-auto">
         <CoverImage title={title} src={coverImage} slug={slug} />
       </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+      <div className="ml-20 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
             <Link
