fe763f10c01de449a8f3f3ea9e0650c06f3cc4c0
Justin Bender <jrkbender@gmail.com>
2024-06-20 21:20:10 -0400

    adding on some 3d cards on the intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,6 +4,7 @@ import JustinImg from '../public/assets/blog/authors/skydiver_justin.jpeg';
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
 import { BackgroundGradient } from './ui/backgroundGradiant';
 import Link from 'next/link';
+import { CardBody, CardContainer, CardItem } from './ui/3dCard';
 
 const Intro = () => {
   const [text] = useTypewriter({
@@ -15,11 +16,11 @@ const Intro = () => {
 
   return (
     <>
-    <section className="flex-col md:flex-row flex items-center md:justify-around mt-8 md:mb-12">
+    <section className="flex-col md:flex-row flex items-center md:justify-around mt-8">
       <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
         <span className="underline decoration-indigo-500">Some</span><span className="text-red-500">(</span>Scripting<span className="text-red-500">)</span><div className='text-sm tracking-wider'>By Justin Bender</div>
       </h1>
-      <div className="flex gap-x-8 text-center text-lg mt-5">
+      <div className="flex gap-x-8 text-center text-lg mt-5 md:mt-0">
           <Link
             title="Visit Justins LinkedIn"
             href="https://www.linkedin.com/in/benderjustin"
@@ -49,26 +50,27 @@ const Intro = () => {
           </Link>
       </div>
     </section>
-      <section className="text-lg flex gap-x-4">
-      <div className='hidden sm:flex w-1/4 justify-end items-end lg:mr-10'>
-        <Image
-          src={JustinImg}
-          style={{objectFit: "fill"}}
-          alt="Picture of Justin Bender"
-          placeholder="blur"
-          className='rounded-full h-40 w-40 md:h-52 md:w-52'
-        />
-        </div>
-        <div className='flex flex-col mt-5 w-full md:w-3/4 mx-auto'>
-        <p className="pb-5 font-semibold">
-          <span className="text-red-500 text-2xl">Hello</span>, my name is <span className="text-indigo-500 text-2xl">Justin</span>
-        </p>
-        <p className="text-sm sm:text-lg pb-5 font-thin">These opinions are my own. Not for any employer that I currently work.</p>
-        <p className="text-sm sm:text-lg font-semibold">All of the posts will be listed below using a timestamp. With the most recent one at the top. If you have any questions, you can message me.</p>
-      </div>
-      </section>
-      <h2 className="ml-0 sm:ml-20 text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold md:pr-8 my-8 md:my-14">
-        
+      <CardContainer className="my-10">
+        <CardBody className="relative group/card hover:shadow-2xl w-full h-auto">
+          <CardItem translateZ="100" className="flex flex-row py-4">
+            <Image
+              src={JustinImg}
+              style={{objectFit: "contain"}}
+              alt="Picture of Justin Bender"
+              placeholder="blur"
+              className='bg-blue-400 rounded-2xl hidden md:block h-52 w-fit lg:ml-20'
+            />
+            <div className='flex flex-col mt-5 w-5/6 md:w-3/4 mx-auto'>
+              <p className="pb-5 font-semibold">
+                <span className="text-red-500 text-2xl">Hello</span>, my name is <span className="text-indigo-500 text-2xl">Justin</span>
+              </p>
+              <p className="text-sm sm:text-lg pb-5 font-thin">These opinions are my own. Not for any employer that I currently work.</p>
+              <p className="text-sm sm:text-lg font-semibold">All of the posts will be listed below using a timestamp. With the most recent one at the top. If you have any questions, you can message me.</p>
+            </div>
+          </CardItem>
+        </CardBody>
+      </CardContainer>
+      <h2 className="ml-0 sm:ml-20 text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold md:pr-8 mb-8 md:mb-14">
         <span>{text}</span><span className="text-indigo-500">?</span>
       </h2>
       </>
