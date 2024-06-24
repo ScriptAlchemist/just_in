4efd3e4e2d7e0ce990adaf6efb50806daff71448
Justin Bender <jrkbender@gmail.com>
2024-06-24 16:08:45 -0400

    remove accidental bg

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -22,7 +22,7 @@ const MoreStories = ({ posts }: Props) => {
   return (
     <section className='mt-10'>
     {!showPosts && <div className='flex w-full justify-end'>
-        <Button onClick={() => setShowPosts(!showPosts)} variant='unstyled' className="bg-blue-200 px-0 w-7/8 md:w-4/6 my-12">
+        <Button onClick={() => setShowPosts(!showPosts)} variant='unstyled' className="px-0 w-7/8 md:w-4/6 my-12">
           <BackgroundGradient containerClassName='w-full' className='bg-black px-4 rounded-3xl flex flex-row items-center text-2xl sm:text-5xl md:text-7xl font-bold tracking-tighter leading-tight'>
             More Posts
             {showPosts ? <Minus className='ml-5 mt-2 h-10 w-10'/> :  <Plus className='ml-5 mt-2 h-10 w-10'/> } 
